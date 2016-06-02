44870f5a40811fb9869c85f8b680c8cea640b93f
pavelvelikhov <pavel.velikhov@gmail.com>
2016-06-02 05:42:14 -0400

    Small change to the driver script

--- src/pythonql/RunPYQL.py
+++ src/pythonql/RunPYQL.py
@@ -4,15 +4,20 @@ from pythonql.Executor import *
 from pythonql.parser.Preprocessor import makeProgram
 import time
 
-if len(sys.argv) != 2:
-  print("Usage: RunPYQL <pythonql program>")
-  sys.exit()
+def runProgramFromFile(fname):
+  before_time = time.time()
+  program = makeProgram(fname)
+  parse_time = time.time()
+  exec(program)
+  exec_time = time.time()
+  return (parse_time-before_time, exec_time-parse_time)
 
-before_time = time.time()
-program = makeProgram( sys.argv[1] )
-parse_time = time.time()
-exec(program)
-exec_time = time.time()
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print("Usage: RunPYQL <pythonql program>")
+    sys.exit()
 
-print("----Time: parsing=%.3f(s), execution=%.3f(s)" % (parse_time-before_time,
-      exec_time-parse_time))
+  (parse_time,exec_time) = runProgramFromFile(fname)
+
+  print("----Time: parsing=%.3f(s), execution=%.3f(s)" % (parse_time,
+      exec_time))
