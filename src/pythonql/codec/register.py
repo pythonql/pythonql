7669ee138bee3a5d8ed60e1d7d6ef343b0f30ae4
Pavel Velikhov <pavel.velikhov@gmail.com>
2016-10-10 00:37:39 +0300

    Added a codec!

---
+++ src/pythonql/codec/register.py
@@ -0,0 +1,54 @@
+import codecs, io, encodings
+import sys
+import traceback
+from encodings import utf_8
+from pythonql.parser.Preprocessor import makeProgramFromString
+
+def pythonql_transform(stream):
+    try:
+        output = "\n" + makeProgramFromString("".join(stream.readlines()))
+    except Exception as ex:
+        print(ex)
+        traceback.print_exc()
+        raise
+
+    return output.rstrip()
+
+def pythonql_transform_string(input):
+    stream = io.StringIO(bytes(input).decode('utf-8'))
+    return pythonql_transform(stream)
+
+def pythonql_decode(input, errors='strict'):
+    return pythonql_transform_string(input), len(input)
+
+class PythonqlIncrementalDecoder(utf_8.IncrementalDecoder):
+    def decode(self, input, final=False):
+        self.buffer += input
+        if final:
+            buff = self.buffer
+            self.buffer = b''
+            return super(PythonqlIncrementalDecoder, self).decode(
+                pythonql_transform_string(buff).encode('utf-8'), final=True)
+        else:
+            return ''
+
+class PythonqlStreamReader(utf_8.StreamReader):
+    def __init__(self, *args, **kwargs):
+        codecs.StreamReader.__init__(self, *args, **kwargs)
+        self.stream = io.StringIO(pythonql_transform(self.stream))
+
+def search_function(encoding):
+    if encoding != 'pythonql': return None
+    # Assume utf8 encoding
+    utf8=encodings.search_function('utf8')
+    return codecs.CodecInfo(
+        name = 'pythonql',
+        encode = utf8.encode,
+        decode = pythonql_decode,
+        incrementalencoder = utf8.incrementalencoder,
+        incrementaldecoder = PythonqlIncrementalDecoder,
+        streamreader = PythonqlStreamReader,
+        streamwriter = utf8.streamwriter)
+
+codecs.register(search_function)
+
