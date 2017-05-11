063162d507a4a788c1a2b6b318b2d9b2b0a9e363
Pavel Velikhov <pavel.velikhov@gmail.com>
2017-05-11 12:50:36 +0300

    Preparing a release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class NoseTestCommand(TestCommand):
         nose.run_exit(argv=['nosetests'])
 
 setup(name='pythonql3',  
-        version='0.9.45',
+        version='0.9.5',
         description='PythonQL Query Language Extension',
         long_description="""
 PythonQL Query Language Extension
